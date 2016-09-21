4696775f76cbe4bb5d9ffbd44c4f83f3e9bc91d0
Ox Cart <ox.to.a.cart@gmail.com>
2016-09-21 16:42:06 -0500

    Added OffsetSize

--- offset.go
+++ offset.go
@@ -4,11 +4,16 @@ import (
 	"encoding/binary"
 )
 
+const (
+	// OffsetSize is the size in bytes of a WAL offset
+	OffsetSize = 16
+)
+
 // Offset records an offset in the WAL
 type Offset []byte
 
 func newOffset(fileSequence int64, position int64) Offset {
-	o := make(Offset, 16)
+	o := make(Offset, OffsetSize)
 	binary.BigEndian.PutUint64(o, uint64(fileSequence))
 	binary.BigEndian.PutUint64(o[8:], uint64(position))
 	return o
